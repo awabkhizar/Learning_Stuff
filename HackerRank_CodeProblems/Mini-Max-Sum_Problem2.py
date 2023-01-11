6b701bb808b8affabe556c7c1839bb62a61a3b4a
Awab <awab_kd@hotmail.com>
2023-01-10 21:26:32 -0500

    Sample Input-Outputs added

--- HackerRank_CodeProblems/Mini-Max-Sum_Problem2.py
+++ HackerRank_CodeProblems/Mini-Max-Sum_Problem2.py
@@ -1,4 +1,8 @@
 #Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+# Sample Input
+# 1 2 3 4 5
+# Sample Output
+# 10 14
 def miniMaxSum(arr):
     # Write your code here
     arr.sort()
--- HackerRank_CodeProblems/Plus-Minus_Problem1.py
+++ HackerRank_CodeProblems/Plus-Minus_Problem1.py
@@ -1,5 +1,17 @@
 #Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
 #Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+# Sample Input
+# STDIN           Function
+# -----           --------
+# 6               arr[] size n = 6
+# -4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
+
+# Sample Output
+# 0.500000
+# 0.333333
+# 0.166667
+
 def plusMinus(arr):
     pos=0
     neg=0
